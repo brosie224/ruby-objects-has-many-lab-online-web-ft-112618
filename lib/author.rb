508ee736142322576118ba3553fad1183399ff8e
Brian Rosengarten <barosengarten@gmail.com>
2018-12-14 17:03:44 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,31 @@
+class Author
+
+  attr_accessor :name
+
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    @posts << post
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    self.posts << post
+    post.author = self
+  end
+
+  self.post_count
+    @@post_count
+  end
+
+end
